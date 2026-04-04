2c0c12a741121e9de10a0d439642b691491bfcf2
RickyUI <rtovarurbina@gmail.com>
2026-04-04 17:32:52 -0500

    Implementacion de main.py con las conexiones a los routers de FastAPI

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 from contextlib import asynccontextmanager
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from langchain_openai import OpenAIEmbeddings
 from app.services.vector_store import VectorStore
-
+from app.routers import upload, query, index
 import os
+
+load_dotenv()
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -20,3 +23,8 @@ async def lifespan(app: FastAPI):
     app.state.vector_store.reset()
 
 app = FastAPI(lifespan=lifespan)
+
+app.include_router(upload.router)
+app.include_router(query.router)
+app.include_router(index.router)
+
